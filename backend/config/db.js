b94f9c95367a5427d3978de75d43e5a2a162a55d
NEELAMOHAN R <neelamohan6cs@gmail.com>
2026-06-21 20:04:20 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 function connectDb() {
     mongoose.connect(process.env.MONGO_DB)
         .then(() => {
-            if (process.env.NODE_ENV === "development") {
+            
                 console.log("MongoDB Connected (DEV MODE)");
-            }
+            
         })
         .catch((err) => {
-            if (process.env.NODE_ENV === "development") {
+           
                 console.log("DB Connection Error:", err);
-            }
+            
         });
 }
 
